1ec10890e40b0ada722536d29fbe2392aecb200c
voodee <eedoov@gmail.com>
2017-04-21 02:13:17 +0300

    add init store

---
+++ src/javascripts/store/initialState.js
@@ -0,0 +1,18 @@
+export default {
+    contacts: [
+        {
+            name: 'Арсений Петров',
+            avatar: 'images/avatar/3.jpg'
+        },
+        {
+            name: 'Наталья Кательникова',
+            avatar: 'images/avatar/2.jpg'
+        }
+    ],
+    message: {
+        '0': {
+            body: 'Привет.',
+            contacts: 0
+        }
+    }
+}
