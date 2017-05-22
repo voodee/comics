c5bdbfcc3cc1912a1328ea8962bd24fda9be2688
voodee <kidala@xaker.ru>
2017-05-22 19:03:22 +0300

    Update initialState.js

--- src/javascripts/store/initialState.js
+++ src/javascripts/store/initialState.js
@@ -29,26 +29,31 @@ export default {
         {
             id: 1,
             messageId: 1,
+            messageIdTo: 2,
             text: 'О чём ты говоришь?'
         },
         {
             id: 2,
             messageId: 1,
+            messageIdTo: 3,
             text: 'Псих, иди в бан!'
         },
         {
             id: 3,
             messageId: 1,
+            messageIdTo: 2,
             text: 'Хорошо, удиви меня :D'
         },
         {
             id: 4,
             messageId: 2,
+            messageIdTo: 3,
             text: 'ответ1'
         },
         {
             id: 5,
             messageId: 2,
+            messageIdTo: 4,
             text: 'ответ2'
         }
     ])
